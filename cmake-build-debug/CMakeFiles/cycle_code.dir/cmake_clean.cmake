8c8dfbcbeded2d0503285b63f06adfeab6ae5b6b
shysa <sanir93@gmail.com>
2019-10-10 14:53:23 +0300

    ver 1.0

---
+++ cmake-build-debug/CMakeFiles/cycle_code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cycle_code.dir/main.cpp.o"
+  "cycle_code"
+  "cycle_code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cycle_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <bitset>
+#include <map>
+
+template <std::size_t  N>
+void vichet(std::bitset<N> result_vector) {
+
+    std::bitset<15> res, obr_polinom("10011");
+
+    obr_polinom <<= 10;
+    std:: cout << "obr polinom: " << obr_polinom << std::endl
+                << "vector delimiy: " << result_vector << std::endl;
+
+    for (int i = 14; i >= 4; i--) {
+        if (result_vector[i] == 1) {
+            result_vector ^= obr_polinom;
+            res <<= 1;
+            res.set(0);
+            obr_polinom >>= 1;
+        }
+        else {
+            obr_polinom >>= 1;
+            res <<= 1;
+        }
+    }
+
+    std::cout << result_vector << std::endl << std::endl;
+
+}
+
+
+template <std::size_t N>
+void get_info(std::bitset<N> e, int v, std::bitset<N> result_vector, std::bitset<N> code_vector) {
+    e = v;
+
+    // Портим информационный вектор
+    result_vector = e ^ code_vector;
+    std::cout << "code: " << code_vector << std::endl <<
+                "e: " << e << std::endl << "result: " << result_vector << std::endl;
+    // Делим порченный информационный вектор на образующий полином
+
+
+    vichet(result_vector);
+    std::cout << "result: " << result_vector;
+
+    //std::cout << result_vector << std::endl;
+}
+
+// Функция заполнения таблички с векторами синдромов s
+// На вход: map<разряд с ошибкой в коде, синдром>
+void fulfillment(std::map<int, std::bitset<5>> *s) {
+
+    for (int i = 1; i <= 15; i++) {
+        s->insert(std::pair<int, std::bitset<5>> (i-1, i));
+    }
+    for (auto it = s->begin(); it != s->end(); it++) {
+        std:: cout << it->first << ":" << it->second << std::endl;
+    }
+
+}
+
+int main() {
+
+    #define size 15
+
+    std::bitset<11> inf_vector("1010011");
+    std::bitset<15> result_vector, e, code_vector("000010100110111");
+    int N[size], Ck[size], Cim[size];
+
+    std::map<int, std::bitset<5>> s;
+    fulfillment(&s);
+
+
+    for (int i = 1; i <= 1; i++) {
+        // Цикл перебора комбинаций ошибок
+        // v - вектор, состоящий из 1 по количеству ошибок n
+        // t - вектор, смещающий крайнюю единицу
+        for (int v = (1 << i) - 1; v <= (1 << size) ; ) {
+            unsigned int t = (v | (v - 1)) + 1;
+            v = t | ((((t & -t) / (v & -v)) >> 1) - 1); //смещение крайней правой единицы влево пока не получится "(1)"
+
+            if (t == 1 << size) {
+                v = (1 << i) - 1;
+                get_info(e, v, result_vector, code_vector);
+                break;
+            }
+
+            //get_info(e, v, result_vector, code_vector);
+        }
+    }
+
+
+
+    return 0;
+}
+
+
